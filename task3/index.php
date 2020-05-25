c2a648732e3084b2e51ac400f455b65c9a3108ef
krs91tk <krs91tk@gmail.com>
2020-05-25 21:14:47 +0900

    task3 done

---
+++ task3/index.php
@@ -0,0 +1,30 @@
+<?php
+//1
+$name = "Takuya";
+if ($name == "Takeya") {
+    echo '私は あなたの名前 です';
+} else {
+    echo 'あなたの名前ではありません';
+}
+
+//2
+for($i = 1; $i <= 10000; $i++) {
+  echo $i;
+  echo "\n";
+}
+
+//3
+$fruits = array("apple", "orange", "melon", "lemon", "banana");
+foreach ($fruits as $value){
+  echo "要素は" . $value;
+  echo "\n";
+}
+//4
+$start = 1;
+$end = 100;
+for ($i = $start;$i < $end; $i++) {
+ if($i % 5 == 0) {
+  echo $i;
+  echo "\n";
+ }
+}
