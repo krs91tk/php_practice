d68f238885084990bf72306109f400b5de0d0871
krs91tk <krs91tk@gmail.com>
2020-09-05 14:02:58 +0900

    modify task3 q2

--- task3/index.php
+++ task3/index.php
@@ -5,13 +5,17 @@ if ($name == "Takeya") {
     echo '私は あなたの名前 です';
 } else {
     echo 'あなたの名前ではありません';
+    echo "\n";
 }
 
 //2
+$total = 0;
 for($i = 1; $i <= 10000; $i++) {
-  echo $i;
-  echo "\n";
+  $total += $i;
 }
+echo $total;
+echo "\n";
+
 
 //3
 $fruits = array("apple", "orange", "melon", "lemon", "banana");
